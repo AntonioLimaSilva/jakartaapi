b39044de983c158d13b498d18dd2ad2aa7d4c07e
Luciano Lima <luclimasilva@hotmail.com>
2020-04-16 17:27:07 -0300

    config hibernate

---
+++ src/main/java/br/com/luciano/jpa/entities/Product.java
@@ -0,0 +1,23 @@
+package br.com.luciano.jpa.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Product {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private Integer id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
---
+++ src/main/java/br/com/luciano/jpa/utils/InitUnitPersistence.java
@@ -0,0 +1,25 @@
+package br.com.luciano.jpa.utils;
+
+import br.com.luciano.jpa.entities.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InitUnitPersistence {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("Ecommerce-PU");
+        EntityManager entityManager = factory.createEntityManager();
+        
+        Product product = entityManager.find(Product.class, 1);
+
+        System.out.println("Name: " + product.getName());
+
+        factory.close();
+        entityManager.close();
+
+    }
+
+}
