06a9a49c53c08d835c14b721468ad5ee8ad4489b
Luciano Lima <luclimasilva@hotmail.com>
2020-04-18 18:46:48 -0300

    teste de cliente persistido

--- src/test/java/br/com/luciano/jpa/merges/CustomerPersistTest.java
+++ src/test/java/br/com/luciano/jpa/merges/CustomerPersistTest.java
@@ -3,20 +3,27 @@ package br.com.luciano.jpa.merges;
 import br.com.luciano.jpa.EntityManagerTest;
 import br.com.luciano.jpa.entities.Customer;
 import br.com.luciano.jpa.entities.Gender;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CustomerPersistTest extends EntityManagerTest {
 
     @Test
-    public void shouldPersistAndSearchCustomer() {
+    public void shouldPersistAndSearchCustomerById() {
 
         Customer customer = new Customer();
         customer.setName("Maria de Souza");
         customer.setGender(Gender.FEMALE);
 
         this.manager.getTransaction().begin();
-        this.manager.persist(customer);
+        customer = this.manager.merge(customer);
         this.manager.getTransaction().commit();
+
+        this.manager.clear();
+
+        Customer customerVerify = this.manager.find(Customer.class, customer.getId());
+
+        Assert.assertEquals(customer.getId(), customerVerify.getId());
 
     }
 
